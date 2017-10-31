c30df0ae0e0c88211174ec5f0f68405c07838268
franck <bigmoletos@yopmail.com>
2017-10-31 14:58:12 +0100

    safariz

---
+++ JQUERY/rechercheBDR5.html.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>idem page1 mais version Ajax encore plus courte</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--<link rel="stylesheet" href="style/todoliste1.css"  type="text/css" charset="utf_8"/>-->
+        <!--<script src="https://code.jquery.com/jquery-1.10.2.js"></script>-->
+        <script  src="https://code.jquery.com/jquery-3.2.1.js"></script>
+        <script src=" https://code.jquery.com/ui/1.11.4/jquery-ui.min.js" ></script>
+
+    </head>
+    <body>
+
+        <p style="border:1px solid blue">Bonjour</p>
+        <button id="disp"> disparaitre </button>
+        <button id = "ap"> apparaitre </button>
+        <button id = "des"> desactiver </button>
+        <script src="jquery.js"></script>
+        <script>
+            ...
+        </script>
+
+
+        <script>
+                    $(document).ready(  function () {
+            });
+            }); //fin document ready
+        </script>
+
+    </body>
+</html>
